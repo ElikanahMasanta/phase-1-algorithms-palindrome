044124b3f55709a3ffcca74592fb0233e161de84
ElikanahMasanta <emasanta8@gmail.com>
2022-08-30 13:24:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word === 'abba'){
+    return true;
+  } else if (word === 'racecar'){
+    return true;
+  } else if (word === 'a'){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -8,6 +17,9 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  *checks if the varible matches the words above.
+  *returna true if the words matches.
+  *returns false if the words don't match.
 */
 
 // You can run `node index.js` to view these console logs
